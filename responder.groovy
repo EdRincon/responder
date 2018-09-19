f0abb13f11ccc9c388b9bf7c3375bb5377ce047e
EdgarRincon <EdgarRincon>
2018-09-19 18:31:32 -0500

    first

---
+++ responder.groovy
@@ -0,0 +1,11 @@
+/*
+* This groovy file responds to the other groovy file
+* to improve mainainance 
+*/
+def hello(greet){
+	if (greet == 'hello') {
+		println 'hello from the other side'
+	} else {
+		println 'wzup'
+	}
+}
